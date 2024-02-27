51eb03e17151229828226600f534ecf842722176
Drew Robson <drewmrobson@gmail.com>
2024-02-27 20:31:46 +1000

    Added functionality to update the category mapping from CLI

--- TransactionCategorization/Program.cs
+++ TransactionCategorization/Program.cs
@@ -1,30 +1,53 @@
 ﻿using nietras.SeparatedValues;
-using TransactionCategorization;
 using System.CommandLine;
 using System.Text.Json;
+using TransactionCategorization;
 
+// 1. File processing command
 var fileOption = new Option<FileInfo?>(
     name: "--file",
     description: "The file to read and display on the console.");
 
+// 2. Update mapping command
+var addCategoryOption = new Option<string[]>(
+        name: "--add-cat",
+        description: "The category to add"
+    )
+{
+    AllowMultipleArgumentsPerToken = true,
+};
+
+// 3. Root config
 var rootCommand = new RootCommand("Sample app for System.CommandLine");
 rootCommand.AddOption(fileOption);
+rootCommand.AddOption(addCategoryOption);
 
-rootCommand.SetHandler((file) =>
+rootCommand.SetHandler((file, map) =>
 {
-    ReadFile(file!);
-},
-    fileOption);
+    if(file != null)
+    {
+        ProcessFile(file!);
+    }
+
+    if(map != null
+        && map.Length == 2)
+    {
+        string mappingText = File.ReadAllText("C:/Source/mapping.json");
+        var categories = JsonSerializer.Deserialize<List<Categories>>(mappingText);
+        categories.Add(new Categories(map[0], map[1]));
+        var data = JsonSerializer.Serialize(categories);
+        File.WriteAllText("C:/Source/mapping.json", data);
+    }
+}, fileOption, addCategoryOption);
 
 await rootCommand.InvokeAsync(args);
 
-static void ReadFile(FileInfo file)
+static void ProcessFile(FileInfo file)
 {
-    //using var reader = Sep.New(',').Reader().FromFile("C:/Source/CSVData.csv");
     using var reader = Sep.New(',').Reader().FromFile(file.FullName);
     using var writer = Sep.New(',').Writer().ToFile("C:/Source/output.csv");
 
-    var list = new List<Model>();
+    var transactions = new List<Model>();
 
     foreach (var readRow in reader)
     {
@@ -32,42 +55,25 @@ static void ReadFile(FileInfo file)
         var amount = readRow[1].ToString().Replace("\"", "");
         var description = readRow[2].ToString();
 
-        var l = new Model
+        transactions.Add(new Model
         {
             Date = DateTime.Parse(date),
             Amount = decimal.Parse(amount),
             Description = description
-        };
-        list.Add(l);
-
-        Console.WriteLine($"{l.Date} {l.Amount} {l.Description}");
+        });
     }
 
     var json = File.ReadAllText("C:/Source/mapping.json");
     var categories = JsonSerializer.Deserialize<List<Categories>>(json);
+    new CategoryParser().Categorise(transactions, categories);
 
-
-
-
-//    var categories = new List<Categories>()
-//{
-//    new Categories("Tatts", "Gambling"),
-//    new Categories("Transfer to CBA A/c NetBank Rent", "Rent"),
-//    new Categories("NERO UTILITIES", "Utilities"),
-//    new Categories("Club Bunker", "Gym")
-//};
-
-
-
-    new CategoryParser().Categorise(list, categories);
-
-    foreach (var w in list)
+    foreach (var item in transactions)
     {
         using var writeRow = writer.NewRow();
-        writeRow["Date"].Set(w.Date.ToString());
-        writeRow["Amount"].Set(w.Amount.ToString());
-        writeRow["Description"].Set(w.Description);
-        writeRow["Category"].Set(w.Category);
+        writeRow["Date"].Set(item.Date.ToString());
+        writeRow["Amount"].Set(item.Amount.ToString());
+        writeRow["Description"].Set(item.Description);
+        writeRow["Category"].Set(item.Category);
     }
 
     writer.Dispose();
