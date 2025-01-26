417bc54a01e6bd1e1311f93bba835334b23ad642
Drew Robson <drewmrobson@gmail.com>
2025-01-26 13:34:02 +1000

    Adding hashing for future matching

--- TransactionCategorization/Model.cs
+++ TransactionCategorization/Model.cs
@@ -1,10 +1,28 @@
-﻿namespace TransactionCategorization
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TransactionCategorization
 {
     internal class Model
     {
         public DateTime Date { get; set; }
         public decimal Amount { get; set; }
-        public string Description { get; set; } = "Unset";
+        public string Description { get; set; } = string.Empty;
         public string Category { get; set; } = "Unset";
+        public string? Hash { get; set; }
+
+        public string CreateMD5()
+        {
+            using (var md5 = MD5.Create())
+            {
+                var input = Encoding.ASCII.GetBytes(
+                    Date.ToString("o", CultureInfo.InvariantCulture) +
+                    Amount.ToString() +
+                    Description);
+                var hash = md5.ComputeHash(input);
+                return Convert.ToHexString(hash);
+            }
+        }
     }
 }
